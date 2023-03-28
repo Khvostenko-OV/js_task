ba72b3bf5475eb3e8d827f5e87f8a66383e61ce2
Oleg Khvostenko <xboctehko@yandex.ru>
2023-03-28 18:09:25 +0200

    Task 6_2 fixed

--- js_task_6_2/task.js
+++ js_task_6_2/task.js
@@ -1,11 +1,14 @@
 const subMenus = Array.from(document.querySelectorAll('.menu_sub')),
-      closeAll = () => subMenus.forEach(menu => menu.className = 'menu menu_sub'),
-      open = win => win.className += ' menu_active';
+      closeAll = () => subMenus.forEach(menu => menu.className = 'menu menu_sub');
 
 for (const item of subMenus) {
     item.closest('.menu__item').getElementsByClassName('menu__link')[0].onclick = () => {
-        closeAll();    
-        open(item);
+        if (item.className.includes('menu_active')) { 
+            item.className = 'menu menu_sub';
+        } else {
+            closeAll();    
+            item.className += ' menu_active';
+        }
         return false;
     }
 }
